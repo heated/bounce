d381b25ff74df16cc575c88102da2b014a5b24b4
Edward Swernofsky <edabsw@gmail.com>
2019-11-26 12:56:21 -0800

    Initial Commit

---
+++ bounce/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  bounce
+//
+//  Created by Edward Swernofsky on 11/26/19.
+//  Copyright © 2019 Edward Swernofsky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
