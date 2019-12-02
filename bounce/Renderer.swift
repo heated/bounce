0426cc291b00ef0c843c7f0b8e9dfbfb2293b097
Edward Swernofsky <edabsw@gmail.com>
2019-12-02 15:20:42 -0800

    wall bounce; cpu physics; 2k 60fps

--- bounce/Renderer.swift
+++ bounce/Renderer.swift
@@ -2,31 +2,43 @@ import Metal
 import MetalKit
 import simd
 
-let width = 800
-let height = 600
-let ballC = 1_000
-let ballDiam = 5.0
+typealias Position = SIMD2<Float>
+typealias Color = SIMD4<Float>
+
+let width: Float = 800
+let height: Float = 600
+let ballC = 2_000
+let ballDiam: Float = 8
 let ballRad = ballDiam / 2
-let ballSpeed = 2.0
+let maxX = width - ballDiam
+let maxY = height - ballDiam
+let ballSpeed: Float = 2
 let maxBuffersInFlight = 3
-let dataUnit = MemoryLayout<Float>.stride
+let dataUnit = MemoryLayout<Position>.stride
+let dataSize = ballC * dataUnit
+
+// TODO: Put balls on GPU; use compute shaders
+
+struct Ball {
+    var x, y, dx, dy: Float
+    init(x: Float, y: Float) {
+        self.x = x
+        self.y = y
+        let angle = Float.random(in: 0..<2*Float.pi)
+        dx = cos(angle) * ballSpeed
+        dy = sin(angle) * ballSpeed
+    }
+}
 
 class Renderer: NSObject, MTKViewDelegate {
     public let device: MTLDevice
     let inFlightSemaphore = DispatchSemaphore(value: maxBuffersInFlight)
     let commandQueue: MTLCommandQueue
-    var depthState: MTLDepthStencilState
     var drawState: MTLRenderPipelineState
     var vertexBuffer: MTLBuffer
+    var colorBuffer: MTLBuffer
     var ballTexture: MTLTexture
-    let vertexData: [Float] = [
-        -1, -1,
-         1, -1,
-        -1,  1,
-         1,  1,
-         1, -1,
-        -1,  1,
-    ]
+    var balls: [Ball] = []
 
     init?(metalKitView: MTKView) {
         self.device = metalKitView.device!
@@ -36,11 +48,6 @@ class Renderer: NSObject, MTKViewDelegate {
         let vertexFunction = library.makeFunction(name: "vertexShader")
         let fragmentFunction = library.makeFunction(name: "fragmentShader")
         
-        metalKitView.depthStencilPixelFormat = MTLPixelFormat.depth32Float_stencil8
-        let depthStateDesciptor = MTLDepthStencilDescriptor()
-        depthStateDesciptor.depthCompareFunction = MTLCompareFunction.less
-        depthStateDesciptor.isDepthWriteEnabled = true
-        self.depthState = device.makeDepthStencilState(descriptor:depthStateDesciptor)!
         do {
             ballTexture = try Renderer.loadTexture(device: device, textureName: "Circle")
         } catch {
@@ -51,17 +58,28 @@ class Renderer: NSObject, MTKViewDelegate {
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.vertexFunction = vertexFunction
         pipelineDescriptor.fragmentFunction = fragmentFunction
-        pipelineDescriptor.depthAttachmentPixelFormat = metalKitView.depthStencilPixelFormat
-        pipelineDescriptor.stencilAttachmentPixelFormat = metalKitView.depthStencilPixelFormat
-        pipelineDescriptor.colorAttachments[0].pixelFormat = metalKitView.colorPixelFormat
+        let attachment = pipelineDescriptor.colorAttachments[0]!
+        attachment.pixelFormat = metalKitView.colorPixelFormat
+        attachment.isBlendingEnabled = true
+        attachment.rgbBlendOperation = .add
+        attachment.alphaBlendOperation = .add
+        attachment.sourceRGBBlendFactor = .one
+        attachment.sourceAlphaBlendFactor = .sourceAlpha
+        attachment.destinationRGBBlendFactor = .oneMinusSourceAlpha
+        attachment.destinationAlphaBlendFactor = .oneMinusSourceAlpha
         
         drawState = try! device.makeRenderPipelineState(descriptor: pipelineDescriptor)
-        
-        let dataSize = vertexData.count * dataUnit;
-        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])!
+        vertexBuffer = device.makeBuffer(length: dataSize)!
+        let colors = (0..<ballC).map { _ -> Color in
+            var color = Color.random(in: 0..<1)
+            color.w = 1
+            return color
+        }
+        colorBuffer = device.makeBuffer(bytes: colors, length: ballC * MemoryLayout<Color>.stride)!
         
         super.init()
         randomizeBalls()
+        copyToVertexBuffer()
     }
     
     class func loadTexture(device: MTLDevice,
@@ -77,19 +95,63 @@ class Renderer: NSObject, MTKViewDelegate {
                                             scaleFactor: 1.0,
                                             bundle: nil,
                                             options: textureLoaderOptions)
-
     }
 
     func randomizeBalls() {
-        
+        for _ in 0..<ballC {
+            var newX, newY: Float
+            
+            repeat {
+                newX = Float.random(in: 0..<maxX)
+                newY = Float.random(in: 0..<maxY)
+            } while balls.contains { ball in
+                let dx = ball.x - newX
+                let dy = ball.y - newY
+                return pow(dx, 2) + pow(dy, 2) < pow(ballDiam, 2)
+            }
+            
+            let ball = Ball(x: newX, y: newY)
+            balls.append(ball)
+        }
     }
     
     func addVerticesForPos(x: Float, y: Float) {
         
     }
     
+    func toCoord(_ x: Float, _ max: Float) -> Float {
+        return 2*(x+ballRad)/max - 1
+    }
+    
+    func isBounded(_ x: Float, _ max: Float) -> Bool {
+        return 0 <= x && x < max
+    }
+    
+    func copyToVertexBuffer() {
+        let contents = vertexBuffer.contents()
+        for i in 0..<ballC {
+            let pos = Position(
+                toCoord(balls[i].x, width),
+                toCoord(balls[i].y, height))
+            
+            contents.storeBytes(of: pos,
+                                toByteOffset: i * dataUnit,
+                                as: Position.self);
+        }
+    }
+    
     func updateGameState() {
-        
+        for i in 0..<ballC {
+            balls[i].x += balls[i].dx
+            balls[i].y += balls[i].dy
+            if !isBounded(balls[i].x, maxX) {
+                balls[i].dx *= -1
+            }
+            if !isBounded(balls[i].y, maxY) {
+                balls[i].dy *= -1
+            }
+        }
+        copyToVertexBuffer()
     }
 
     func draw(in view: MTKView) {
@@ -112,12 +174,9 @@ class Renderer: NSObject, MTKViewDelegate {
                 /// Final pass rendering code here
                 if let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) {
                     renderEncoder.setRenderPipelineState(drawState)
-                    renderEncoder.setCullMode(.back)
-                    renderEncoder.setFrontFacing(.counterClockwise)
-                    renderEncoder.setDepthStencilState(depthState)
-                    renderEncoder.setFragmentTexture(ballTexture, index: TextureIndex.color.rawValue)
-                    renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-                    renderEncoder.drawPrimitives(type: MTLPrimitiveType.triangle, vertexStart: 0, vertexCount: 6)
+                    renderEncoder.setFragmentTexture(ballTexture, index: 0)
+                    renderEncoder.setVertexBuffers([vertexBuffer, colorBuffer], offsets: [0, 0], range: 0..<2)
+                    renderEncoder.drawPrimitives(type: MTLPrimitiveType.point, vertexStart: 0, vertexCount: ballC)
                     renderEncoder.endEncoding()
                     
                     if let drawable = view.currentDrawable {
--- bounce/Shaders.metal
+++ bounce/Shaders.metal
@@ -12,43 +12,56 @@
 #include <simd/simd.h>
 
 // Including header shared between this Metal shader code and Swift/C code executing Metal API commands
-#import "ShaderTypes.h"
+// #import "ShaderTypes.h"
 
 using namespace metal;
 
-typedef struct
-{
-    float3 position [[attribute(VertexAttributePosition)]];
-    float2 texCoord [[attribute(VertexAttributeTexcoord)]];
-} Vertex;
+struct Ball {
+    float2 pos;
+    float2 vel;
+};
 
-typedef struct
-{
-    float4 position [[position]];
-    float2 texCoord;
-} ColorInOut;
+struct VertexOut {
+    float4 position [[ position ]];
+    float point_size [[ point_size ]];
+    float4 color;
+};
 
-vertex ColorInOut vertexShader(Vertex in [[stage_in]],
-                               constant Uniforms & uniforms [[ buffer(BufferIndexUniforms) ]])
-{
-    ColorInOut out;
+bool isBounded(float x, float max) {
+    return 0 <= x && x < max;
+}
 
-    float4 position = float4(in.position, 1.0);
-    out.position = uniforms.projectionMatrix * uniforms.modelViewMatrix * position;
-    out.texCoord = in.texCoord;
+kernel void moveBalls(device Ball* balls,
+                      uint i [[ thread_position_in_grid ]]) {
+    balls[i].pos += balls[i].vel;
+    
+    if (!isBounded(balls[i].pos.x, 800)) {
+        balls[i].vel.x *= -1;
+    }
+    
+    if (!isBounded(balls[i].pos.y, 600)) {
+        balls[i].vel.y *= -1;
+    }
+}
 
+vertex VertexOut vertexShader(const device float2* vertices,
+                              const device float4* colors,
+                              uint i [[ vertex_id ]]) {
+    VertexOut out;
+    out.position = float4(vertices[i], 0, 1);
+    out.color = colors[i];
+    out.point_size = 8;
     return out;
 }
 
-fragment float4 fragmentShader(ColorInOut in [[stage_in]],
-                               constant Uniforms & uniforms [[ buffer(BufferIndexUniforms) ]],
-                               texture2d<half> colorMap     [[ texture(TextureIndexColor) ]])
-{
+fragment float4 fragmentShader(VertexOut in [[ stage_in ]],
+                               float2 pos [[ point_coord ]],
+                               texture2d<float> circle [[ texture(0) ]]) {
     constexpr sampler colorSampler(mip_filter::linear,
                                    mag_filter::linear,
                                    min_filter::linear);
 
-    half4 colorSample   = colorMap.sample(colorSampler, in.texCoord.xy);
+    float4 sample = circle.sample(colorSampler, pos) * in.color;
 
-    return float4(colorSample);
+    return float4(sample);
 }
